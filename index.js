d0c459d33c5a8f8dbbb66b727f11afebeac8a509
HrridoyV2 <hrridoyv2@gmail.com>
2020-11-17 12:33:49 +0600

    Error with file

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const ObjectId = require("mongodb").ObjectId;
+const fileUpload = require("express-fileupload");
+const fs = require("fs-extra");
 const MongoClient = require("mongodb").MongoClient;
 const uri =
   "mongodb+srv://hridoy:Team49@cluster0.smtok.mongodb.net/apartment-hunt?retryWrites=true&w=majority";
@@ -15,6 +17,9 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static("service"));
+app.use(fileUpload());
+
 const port = 5000;
 
 app.get("/", (req, res) => {
@@ -24,37 +29,45 @@ app.get("/", (req, res) => {
 
 client.connect((err) => {
   const rentsCollection = client.db("apartment-hunt").collection("items");
-  
+
   const ordersCollection = client.db("apartment-hunt").collection("orders");
   // perform actions on the collection object
-  
+
   //Send RentsData from server to Homepage
   app.get("/rentData", (req, res) => {
-    rentsCollection.find({})
-    .toArray((err, documents) => {
+    rentsCollection.find({}).toArray((err, documents) => {
       res.send(documents);
-    })
+    });
   });
   //
-    app.get("/home/:id", (req, res) => {
-      rentsCollection
-        .find({ _id: ObjectId(req.params.id) })
-        .toArray((err, documents) => {
-          res.send(documents[0]);
-        });
-    });    
-  //
-    app.post('/orderRent', (req, res) => {
-      const order = req.body;
-      ordersCollection.insertOne(order)
-      .then((result) => {
-        res.send(result.insertedCount > 0);
+  app.get("/home/:id", (req, res) => {
+    rentsCollection
+      .find({ _id: ObjectId(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
       });
-    })
+  });
   //
-
-
-
+  app.post("/orderRent", (req, res) => {
+    const order = req.body;
+    ordersCollection.insertOne(order).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+  //
+  // loading all orders
+  app.get("/allOrders", (req, res) => {
+    ordersCollection.find({})
+    .toArray((err, docs) => res.send(docs));
+  });
+  // loading orders by email
+  app.get("/myRents/:email", (req, res) => {
+    ordersCollection
+      .find({ email: req.params.email })
+      .toArray((err, docs) => res.send(docs));
+  });
+  // adding new service
+    
   console.log("db connected");
 });
 
